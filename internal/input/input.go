055307aabfda19f1ef249592ad90cb0551bd9c1d
Bimba Angapov <bimbangapov@yandex.ru>
2026-05-30 12:08:32 +0300

    Удалил лишнее сканирование строк

--- internal/input/input.go
+++ internal/input/input.go
@@ -3,25 +3,31 @@ package input
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func InputWeightHeight() (float64, float64, error) {
 	var weight, height float64
+	var inputWeight, inputHeight string
 
 	fmt.Print("Введите свой вес в килограммах: ")
-	_, err := fmt.Scan(&weight)
+	fmt.Scan(&inputWeight)
+	inputWeight = strings.ReplaceAll(inputWeight, ",", ".")
+	weight, err := strconv.ParseFloat(inputWeight, 64)
 	if err != nil {
-		return 0, 0, errors.New("Вес должен быть числом")
+		return 0, 0, err
 	}
-
 	if weight <= 0 {
 		return 0, 0, errors.New("Рост/вес должны быть положительными")
 	}
 
 	fmt.Print("Введите свой рост в сантиметрах: ")
-	_, err = fmt.Scan(&height)
+	fmt.Scan(&inputHeight)
+	inputHeight = strings.ReplaceAll(inputHeight, ",", ".")
+	height, err = strconv.ParseFloat(inputHeight, 64)
 	if err != nil {
-		return 0, 0, errors.New("Рост должен быть числом")
+		return 0, 0, err
 	}
 
 	height = height / 100
